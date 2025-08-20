d0a3d053551e9985145fcda3fc3eede591536682
aish1998 <31594683+aish1998@users.noreply.github.com>
2025-08-21 02:53:09 +0530

    Add files via upload

---
+++ 1) Basics/function_practice.py
@@ -0,0 +1,15 @@
+
+# Function to find sum of 3 numbers
+def find_sum(a, b, c):
+    return a + b + c
+
+
+def find_average(a, b, c):
+    return (a + b + c) / 3
+
+
+num1 = 10
+num2 = 20
+num3 = 30
+print("Sum of numbers:", find_sum(num1, num2, num3))
+print("Average of numbers:", find_average(num1, num2, num3))
